f6eafe59660b2022bba3c8cb58eda9568e03bf9e
Ryan Papsin <ryanpapsin175@gmail.com>
2022-11-14 15:50:31 -0700

    first sign of life

---
+++ js/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+let recipeArray = [];
+
+let recipeContainer = document.getElementById('recipeContainer');
+
+let selectorRecipe = document.getElementById('selectorRecipe');
+
+let recipeName = document.getElementById('recipeName');
+
+let recipeIng = document.getElementById('recipeIng');
+
+function Recipe(name, ingredients, fileExtension) {
+  this.name = name;
+  this.ingredients = ingredients;
+  this.imagePath = `img/${name}.${fileExtension}`;
+}
+
+Recipe.prototype.render = function () {
+  let ulElem = document.createElement('ul');
+  recipeContainer.appendChild(ulElem);
+  let liElem = document.createElement('li');
+  liElem.textContent = this.name;
+  ulElem.appendChild(liElem);
+
+
+  recipeArray.push(this);
+}
+
+
+let test = new Recipe('hello');
+
+test.render();
