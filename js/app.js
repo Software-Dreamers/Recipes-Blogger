86bf9a118318633c94df5a590380575fa58ebc78
SHARMARKE NUR <Snur206@yahoo.com>
2022-11-15 10:53:29 -0800

    trying to pull staging

--- js/app.js
+++ js/app.js
@@ -12,10 +12,12 @@ let recipeName = document.getElementById('recipeName');
 
 let recipeIng = document.getElementById('recipeIng');
 
-function Recipe(name, ingredients, img) {
+function Recipe(name, ingredients, details, fileExtension = 'jpeg') {
   this.name = name;
   this.ingredients = ingredients;
-  this.img = img ;
+  this.details = details;
+  this.fileExtension = fileExtension;
+  this.src = `img/${name}.${fileExtension}`;
   recipeArray.push(this);
 }
 
@@ -52,10 +54,11 @@ function handleClick(event) {
 }
 
 
-let mac = new Recipe('Mac & Cheese', ['Pasta', 'Cheese', 'Milk', 'Butter','Salt'],'img/macNcheese.jpeg');
+let mac = new Recipe('Mac & Cheese', ['Pasta', 'Cheese', 'Milk', 'Butter','Breadcrumbs', 'Salt', 'Pepper'] 'img/macNcheese.jpeg');
 
 let burger = new Recipe('Cheese Burger', ['Bun', 'Patty', 'Cheese', 'Tomato', 'Ketchup', 'Mustard', 'Pickle', 'Lettuce'],'img/cheese-burger.jpeg');
 
+if
 
 function renderList() {
   for (let i = 0; i < recipeArray.length; i++) {
@@ -65,6 +68,27 @@ function renderList() {
     recipeIng.appendChild(liElem);
   }
 }
+
+// let allMeat = new Food 'beef', 'chicken', 'egg', 'beans', 'steak']
+// let allElse = new Food('lettuce', 'onion', 'tomato', 'peppers', 'lime', 'lemon', 'corn', 'butter']
+// let allItems = ['noodles', 'breadcrumbs', 'buns', 'tortilla', 'rice']
+// let allSpices = ['salt', 'pepper']
+
+// let food = [allMeat, allElse, allItems, allSpices];
+
+// for (let i = o; i < food.length; i++) {
+//   for (let j = 0; < food[i].length; j++) {
+
+//   }
+
+}
+
+
+for (let i = 0; )
+
+
+
+
 
 renderList();
 
