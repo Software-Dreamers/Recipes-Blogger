f9b1e252605c3c442bc60f2d3f4d5760272176a9
Ryan Papsin <ryanpapsin175@gmail.com>
2022-11-15 16:59:06 -0700

    hopeuly it will work

--- js/app.js
+++ js/app.js
@@ -14,12 +14,12 @@ let recipeIng = document.getElementById('recipeIng');
 
 
 
-function Recipe(name, ingredients, img,prepTime, cookTime,description) {
+function Recipe(name, ingredients, img, prepTime, cookTime, description) {
   this.name = name;
   this.ingredients = ingredients;
 
- 
-  this.img = img ;
+
+  this.img = img;
   this.description = description;
   this.prepTime = prepTime;
   this.cookTime = cookTime;
@@ -32,14 +32,14 @@ let clickRecipe;
 function handleClick(event) {
   let index = event.target.id;
   console.log(index);
- 
+
   clickRecipe = recipeArray[index];
 
   while (selectorRecipe.firstChild) {
     selectorRecipe.removeChild(selectorRecipe.firstChild);
   }
 
-  
+
 
   let heading = document.createElement('h2');
   heading.textContent = clickRecipe.name;
@@ -47,15 +47,15 @@ function handleClick(event) {
 
 
   let ingredientHeading = document.createElement('ingredientHeading');
-  ingredientHeading.innerHTML = '<br />' + " Ingredients";
+  ingredientHeading.innerHTML = '<br />' + 'Ingredients';
   heading.appendChild(ingredientHeading);
 
 
-  
+
   let imgElem = document.createElement('img');
   imgElem.src = clickRecipe.img;
   selectorRecipe.appendChild(imgElem);
- 
+
 
   let prepTime = document.createElement('h4');
   prepTime.innerHTML = 'Prep Time: ' + clickRecipe.prepTime;
@@ -67,13 +67,9 @@ function handleClick(event) {
   selectorRecipe.appendChild(cookTime);
 
 
-  let ingredientHeading = document.createElement('h3');
-  ingredientHeading.innerHTML = "Ingredients";
- 
   selectorRecipe.appendChild(ingredientHeading);
 
 
- 
   let ulElem = document.createElement('ul');
   selectorRecipe.appendChild(ulElem);
 
@@ -84,24 +80,24 @@ function handleClick(event) {
   }
 
   createForm();
-}
-function createForm() {
-  let container = document.getElementById('formContainer');
-  let form = document.createElement('FORM');
-  let commentBox = document.createElement('input');
-  commentBox.setAttribute('type', 'text');
-  commentBox.setAttribute('id', 'commentBox');
-  commentBox.setAttribute('placeholder', 'Enter Comment');
-  form.appendChild(commentBox);
-  let submit = document.createElement('button');
-  submit.setAttribute('type', 'submit');
-  submit.setAttribute('name', 'cmtBtn');
-  form.appendChild(submit);
-  container.appendChild(form);
-  submit.addEventListener('click', function (event) {
-    event.preventDefault();
-  });
-}
+
+  function createForm() {
+    let container = document.getElementById('formContainer');
+    let form = document.createElement('FORM');
+    let commentBox = document.createElement('input');
+    commentBox.setAttribute('type', 'text');
+    commentBox.setAttribute('id', 'commentBox');
+    commentBox.setAttribute('placeholder', 'Enter Comment');
+    form.appendChild(commentBox);
+    let submit = document.createElement('button');
+    submit.setAttribute('type', 'submit');
+    submit.setAttribute('name', 'cmtBtn');
+    form.appendChild(submit);
+    container.appendChild(form);
+    submit.addEventListener('click', function (event) {
+      event.preventDefault();
+    });
+  }
 
 
 
@@ -120,10 +116,10 @@ function createForm() {
 }
 
 
-let mac = new Recipe('Mac & Cheese', ['Pasta', 'Cheese', 'Milk', 'Butter','Salt'],'img/macNcheese.jpeg','10 min','45 min','Boil the macaroni in salted water until the noodles are al dente. Drain and transfer to a prepared baking dish.Then Melt butter, then whisk in the flour. Whisk in the milk, bring to a simmer, and stir in the cheeses. Season with salt and pepper and continue simmering until the sauce is thick. Pour the sauce over the noodles and stir.Melt two tablespoons of butter in a skillet, add the bread crumbs, and toast until the crumbs are brown. Spread the topping over the macaroni and cheese, then sprinkle with paprika.Bake in the preheated oven(350 degrees F) until the topping is golden brown.');
+let mac = new Recipe('Mac & Cheese', ['Pasta', 'Cheese', 'Milk', 'Butter', 'Salt'], 'img/macNcheese.jpeg', '10 min', '45 min', 'Boil the macaroni in salted water until the noodles are al dente. Drain and transfer to a prepared baking dish.Then Melt butter, then whisk in the flour. Whisk in the milk, bring to a simmer, and stir in the cheeses. Season with salt and pepper and continue simmering until the sauce is thick. Pour the sauce over the noodles and stir.Melt two tablespoons of butter in a skillet, add the bread crumbs, and toast until the crumbs are brown. Spread the topping over the macaroni and cheese, then sprinkle with paprika.Bake in the preheated oven(350 degrees F) until the topping is golden brown.');
 
 
-let burger = new Recipe('Cheese Burger', ['Bun', 'Patty', 'Cheese', 'Tomato', 'Ketchup', 'Mustard', 'Pickle', 'Lettuce'],'img/cheese-burger.jpeg','15 min','40 min');
+let burger = new Recipe('Cheese Burger', ['Bun', 'Patty', 'Cheese', 'Tomato', 'Ketchup', 'Mustard', 'Pickle', 'Lettuce'], 'img/cheese-burger.jpeg', '15 min', '40 min');
 
 
 
@@ -150,20 +146,20 @@ const getLikeNum = document.querySelector('.likeNum');
 
 
 
-function likeIncrement (){
+function likeIncrement() {
   console.log(clickRecipe);
-  clickRecipe.likes ++;
+  clickRecipe.likes++;
   getLikeNum.innerHTML = `${clickRecipe.likes}`;
 }
 
-getLike.addEventListener('click',likeIncrement);
+getLike.addEventListener('click', likeIncrement);
 
 let food = localStorage.getItem('myFood');
 
 
-function saveFood(){
+function saveFood() {
   let foodString = JSON.stringify(recipeArray);
-  localStorage.setItem('food',foodString );
+  localStorage.setItem('food', foodString);
 
 }
 
